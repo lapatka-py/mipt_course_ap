edac100b9eeacd3c23be4285b78eb05eee4af2c2
lapatka-py <kozlov.ds@phystech.edu>
2020-12-21 19:40:26 +0700

    homework6

--- homework8/ex_5_w8.py
+++
@@ -1,6 +0,0 @@
-from itertools import permutations
-
-
-def get_permutations(s, n):
-    return sorted([''.join(x) for x in permutations(s, n)])
-
--- homework8/ex_9_w8.py
+++
@@ -1,8 +0,0 @@
-from itertools import accumulate
-
-
-def maximize(lists, m):
-    squared_maxes = [max(lst) ** 2 for lst in lists]
-    polynom = [p for p in accumulate(squared_maxes)]
-    return max([x % m for x in polynom])
-
